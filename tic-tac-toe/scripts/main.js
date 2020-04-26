ae55c71b024aec0fadb78320da486cbf23bf09b4
3ZA <jasonchc@hotmail.co.uk>
2020-04-26 19:47:35 +0100

    populate cells with markers on click

--- tic-tac-toe/scripts/main.js
+++ tic-tac-toe/scripts/main.js
@@ -7,13 +7,43 @@ const Player = (name) => {
   return { name };
 };
 
-x = Gameboard();
-console.log(x.gameboard);
+var currentBoard;
+var grid = document.getElementById('grid');
+var gameboard = [].slice.call(grid.querySelectorAll('.square'), 0);
 
-//var squares = document.getElementsByClassName('square');
-//for (let i = 0; i < squares.length; i++) {
-//  squares[i].textContent = i;
-//}
-//new game creates a gameboard(player1, player2)
-//players are instances of a Player(name)
-//render each element of the array in a grid
+var moves = ['X', 'O'];
+moveToNum = new Map([
+  ['X', -1],
+  ['O', 1],
+]);
+curr = 0;
+grid.addEventListener('click', playMove);
+function playMove(e) {
+  var index = gameboard.indexOf(e.target);
+  divIndex = index + 1;
+  var square = document.querySelector(
+    `.square:nth-child(n + ${divIndex}):nth-child(n - ${divIndex})`
+  );
+  if (currentBoard.gameboard[index] !== 0) {
+    return;
+  }
+  currentBoard.gameboard[index] = moveToNum.get(moves[curr]);
+  square.textContent = moves[curr];
+  checkBoard();
+  curr = 1 - curr;
+}
+
+function startGame() {
+  console.log('new game started');
+  currentBoard = Gameboard();
+  var squares = Array.from(document.getElementsByClassName('square')).forEach(
+    (item) => {
+      item.textContent = '';
+    }
+  );
+}
+
+function checkBoard() {
+  board = currentBoard.gameboard;
+}
+startGame();
